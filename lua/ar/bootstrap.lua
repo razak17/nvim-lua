903c8d400e766084f8a0379784b2a51e2474a84e
Razak Mo <moabdulrazak17@gmail.com>
2024-08-01 21:56:17 +0000

    feat: remove padding around neovim instance

--- lua/ar/bootstrap.lua
+++ lua/ar/bootstrap.lua
@@ -320,3 +320,20 @@ vim.o.guifont = "Operator Mono SSm Lig Book:h9" -- text below applies for VimScr
 vim.g.neovide_input_macos_alt_is_meta = true
 vim.opt.linespace = 0
 end
+
+-- https://www.reddit.com/r/neovim/comments/1ehidxy/you_can_remove_padding_around_neovim_instance/
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    -- io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+    io.write(string.format("\027Ptmux;\027\027]11;#%06x\007\027\\", normal.bg)) -- tmux
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function()
+    -- io.write("\027]111\027\\")
+    io.write("\027Ptmux;\027\027]111;\007\027\\") -- tmux
+  end,
+})
