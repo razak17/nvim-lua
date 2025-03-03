7598a5bc666fa5c6f0be723db3791e37599d8322
Abdul Razak <moabdulrazak17@gmail.com>
2025-03-03 05:45:12 +0000

    feat(mappings): force quit all

--- lua/ar/mappings.lua
+++ lua/ar/mappings.lua
@@ -1,5 +1,6 @@
 local nnoremap = function(...) map('n', ...) end
 map('n', '<leader>qq', '<cmd>q<CR>')
+nnoremap('<leader>Q', ':qa!<CR>', { desc = 'force quit all', silent = true })
 map('n', 'H', '<cmd>bprevious<CR>', { desc = 'previous buffer' })
 map('n', 'L', '<cmd>bnext<CR>', { desc = 'next buffer' })
 map('n', '<leader>c', '<cmd>bdel<CR>', { desc = 'delete buffer' })
